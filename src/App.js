f4cfa3e3ea088e77aa4988f79004968cfaf68c2f
Rahmani Seif El Moulouk <cool2309@gmail.com>
2018-10-02 00:26:48 +0200

    Improving the UI using TailwindCSS

--- src/App.js
+++ src/App.js
@@ -1,64 +1,66 @@
 // Import components
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import DisplayTodos from './Components/DisplayTodos';
 import AddTodo from './Components/AddTodo';
-
 // Import styles
 import './App.css';
 
 
 class App extends Component {
 
-  state = {
-    todoList : [
-      {title: "First Todo", body: "This is the first todo", status: "remaining", id: 1},
-      {title: "Success Todo", body: "This todo is done", status: "done", id: 2},
-      {title: "Fail Todo", body: "This is the first todo", status: "fail", id: 3}
-    ],
-  }
+    state = {
+        todoList: [
+            {title: "First Todo", body: "This is the first todo", status: "remaining", id: 1},
+            {title: "Success Todo", body: "This todo is done", status: "done", id: 2},
+            {title: "Fail Todo", body: "This is the first todo", status: "fail", id: 3}
+        ],
+    }
 
-  // Add a todo
-  addTodo = (todo) => {
-    todo.id = Math.random();
-    const newTodoList = [todo, ...this.state.todoList]
-    this.setState({
-      todoList: newTodoList,
-    });
-  };
+    // Add a todo
+    addTodo = (todo) => {
+        todo.id = Math.random();
+        const newTodoList = [todo, ...this.state.todoList]
+        this.setState({
+            todoList: newTodoList,
+        });
+    };
 
-  // Change the status of the todo
-  changeStatus = (id, status) => {
-    const newTodoList = this.state.todoList.map((todo) => {
-      if (todo.id == id) {
-        todo.status = status;
-      }
-      return todo;
-    });
+    // Change the status of the todo
+    changeStatus = (id, status) => {
+        const newTodoList = this.state.todoList.map((todo) => {
+            if (todo.id === id) {
+                todo.status = status;
+            }
+            return todo;
+        });
 
-    this.setState({
-      todoList: newTodoList,
-    });
-  };
+        this.setState({
+            todoList: newTodoList,
+        });
+    };
 
-  // Delete a todo
-  deleteTodo = (id) => {
-    const newTodoList = this.state.todoList.filter((todo) => {
-      return (todo.id !== id);
-    });
+    // Delete a todo
+    deleteTodo = (id) => {
+        const newTodoList = this.state.todoList.filter((todo) => {
+            return (todo.id !== id);
+        });
 
-    this.setState({
-      todoList: newTodoList,
-    });
-  }
+        this.setState({
+            todoList: newTodoList,
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <AddTodo addTodo = {this.addTodo}/>
-        <DisplayTodos deleteTodo={ this.deleteTodo } changeStatus={ this.changeStatus } todoList = { this.state.todoList } />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="h-screen w-full flex items-center justify-center bg-yellow-lighter font-sans">
+                <div className="bg-white rounded-lg shadow-lg p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+                    <AddTodo addTodo={this.addTodo}/>
+                    <DisplayTodos deleteTodo={this.deleteTodo} changeStatus={this.changeStatus}
+                                  todoList={this.state.todoList}/>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default App;
--- src/Components/DisplayTodos.js
+++ src/Components/DisplayTodos.js
@@ -1,22 +1,44 @@
 import React from 'react';
 
 const DisplayTodos = ({todoList, changeStatus, deleteTodo}) => {
-    if ( todoList.length !== 0 ) {
+    if (todoList.length !== 0) {
         const todoDisplay = todoList.map((todo) => {
-            return(
-                <div className={todo.status} >
-                    <h2 className="todoTitle">{todo.title}</h2>
-                    <p className="todoBody">{todo.body}</p>
-                    <button class="doneButton btn" onClick={() => {changeStatus(todo.id, "done")}}>Done</button>
-                    <button class="failButton btn" onClick={() => {changeStatus(todo.id, "fail")}}>Not Done</button>
-                    <button class="deleteButton btn" onClick={() => {deleteTodo(todo.id)}}>Delete Todo</button>
+            return (
+                <div className="w-1/2 px-3 my-3" key={todo.id}>
+                    <div className={todo.status + " rounded shadow-lg"}>
+                        <div className="px-6 py-4">
+                            <div className="font-bold text-xl mb-2">{todo.title}</div>
+                            <p className="text-base">{todo.body}</p>
+                        </div>
+                        <div className="px-6 py-4">
+                            <button
+                                className="bg-blue hover:bg-blue-dark text-white font-bold text-xs py-2 px-4 rounded-full mr-3"
+                                onClick={() => {
+                                    changeStatus(todo.id, "done")
+                                }}>Done
+                            </button>
+                            <button
+                                className="bg-white hover:bg-grey-lightest text-grey-darkest font-semibold py-2 px-4 border border-grey-light rounded-full text-xs shadow mr-3"
+                                onClick={() => {
+                                    changeStatus(todo.id, "fail")
+                                }}>Not Done
+                            </button>
+                            <button
+                                className="bg-red hover:bg-red-dark text-white font-bold text-xs py-2 px-4 rounded-full"
+                                onClick={() => {
+                                    deleteTodo(todo.id)
+                                }}>Delete Todo
+                            </button>
+                        </div>
+                    </div>
                 </div>
             );
         });
 
-        return(<div class="todoCont" >{todoDisplay}</div>);
+        return (<div className="px-3 mb-4"> <div className="flex flex-wrap -mx-3">{todoDisplay}</div></div>);
     } else {
-        return(<div className = "todoCont"><h1 class="noTodo">No Todos to display. Add some todos from above form</h1></div>);
+        return (<h1 className="text-grey-darkest text-center">No Todos to display. Add some todos from above
+            form</h1>);
     }
 }
 
